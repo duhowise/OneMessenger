a90f1730183b7b6cea0bd5fea22e76e5fbdb3b02
duho wise <wsduho>
2017-06-06 13:59:47 +0000

    Added Server Solution

---
+++ OneMessenger/Src/OneMessenger.Server/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneMessenger.Server
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
